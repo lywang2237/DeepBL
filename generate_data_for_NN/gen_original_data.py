897a02a6466e907b918210c5a8661a9c5c65e4c0
lywang2237 <105486572+lywang2237@users.noreply.github.com>
2022-06-14 20:28:18 +0800

    Update gen_original_data.py

--- generate_data_for_NN/gen_original_data.py
+++ generate_data_for_NN/gen_original_data.py
@@ -18,10 +18,10 @@ turflux_std_all = np.load(input_path + 'turflux_std_all.npy')
 
 # normalization
 for n in range(input_dim):
-	data_in[:,:,n] = (data_in[:,:,n]-large_avg_all[n])/large_std_all[n]
+    data_in[:,:,n] = (data_in[:,:,n]-large_avg_all[n])/large_std_all[n]
 
 for n in range(output_dim):
-	data_out[:,:,n] = (data_out[:,:,n]-turflux_avg_all[n])/turflux_std_all[n]
+    data_out[:,:,n] = (data_out[:,:,n]-turflux_avg_all[n])/turflux_std_all[n]
 
 # save
 np.save(output_path+'data_in',data_in)
