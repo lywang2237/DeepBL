63a0216787ed5901a299d711b4fe9cdcf7135340
lywang2237 <105486572+lywang2237@users.noreply.github.com>
2022-06-14 20:36:27 +0800

    Update gen_raw_data.py

--- generate_data_for_NN/gen_raw_data.py
+++ generate_data_for_NN/gen_raw_data.py
@@ -25,9 +25,9 @@ ry = np.floor(ny/2)
 # get all the valid coordinates that are in the circular region
 valid_list = []
 for i in range(nx):
-	for j in range(ny):
-		if (rx-i)**2+(ry-j)**2 <= radius**2:
-			valid_list.append([i,j])
+    for j in range(ny):
+        if (rx-i)**2+(ry-j)**2 <= radius**2:
+            valid_list.append([i,j])
 
 # length of this list is the data number of a single time output
 num_per_frame = len(valid_list)
@@ -41,44 +41,44 @@ data_out = np.zeros((num_per_frame*9*6,nz,4))
 # transform the original data into training/validation/test data
 frame_count = 0
 for hid in range(14,24):
-	for mid in range(0,6):
+    for mid in range(0,6):
 
-		if hid == 19 and mid == 3:
-			continue
-		
-		frame_count += 1
-		TURFLUX = Dataset(input_path+'avg_flux_all_'+str(hid)+':'+str(mid)+'0:00.nc')
-		LARGE = Dataset(input_path+'avg_large_'+str(hid)+':'+str(mid)+'0:00.nc')
-		uw = TURFLUX.variables['uw'][:]
-		vw = TURFLUX.variables['vw'][:]
-		thw = TURFLUX.variables['thw'][:]
-		qvw = TURFLUX.variables['qvw'][:]
-		u = LARGE.variables['u'][:]
-		v = LARGE.variables['v'][:]
-		w = LARGE.variables['w'][:]
-		th = LARGE.variables['th'][:]
-		qv = LARGE.variables['qv'][:]
+        if hid == 19 and mid == 3:
+            continue
+
+        frame_count += 1
+        TURFLUX = Dataset(input_path+'avg_flux_all_'+str(hid)+':'+str(mid)+'0:00.nc')
+        LARGE = Dataset(input_path+'avg_large_'+str(hid)+':'+str(mid)+'0:00.nc')
+        uw = TURFLUX.variables['uw'][:]
+        vw = TURFLUX.variables['vw'][:]
+        thw = TURFLUX.variables['thw'][:]
+        qvw = TURFLUX.variables['qvw'][:]
+        u = LARGE.variables['u'][:]
+        v = LARGE.variables['v'][:]
+        w = LARGE.variables['w'][:]
+        th = LARGE.variables['th'][:]
+        qv = LARGE.variables['qv'][:]
         qc = LARGE.variables['qc'][:]
         
         # get all the data points in the circular region into the data_in and data_out
-		for i in range(num_per_frame):
-			data_in[num_per_frame*(frame_count-1)+i,:,0] = u[:,valid_list[i][1],valid_list[i][0]]
-			data_in[num_per_frame*(frame_count-1)+i,:,1] = v[:,valid_list[i][1],valid_list[i][0]]
-			data_in[num_per_frame*(frame_count-1)+i,:,2] = w[:,valid_list[i][1],valid_list[i][0]]
-			data_in[num_per_frame*(frame_count-1)+i,:,3] = th[:,valid_list[i][1],valid_list[i][0]]
-			data_in[num_per_frame*(frame_count-1)+i,:,4] = qv[:,valid_list[i][1],valid_list[i][0]]
-			data_out[num_per_frame*(frame_count-1)+i,:,0] = uw[:,valid_list[i][1],valid_list[i][0]]
-			data_out[num_per_frame*(frame_count-1)+i,:,1] = vw[:,valid_list[i][1],valid_list[i][0]]
-			data_out[num_per_frame*(frame_count-1)+i,:,2] = thw[:,valid_list[i][1],valid_list[i][0]]
-			data_out[num_per_frame*(frame_count-1)+i,:,3] = qvw[:,valid_list[i][1],valid_list[i][0]]
+        for i in range(num_per_frame):
+            data_in[num_per_frame*(frame_count-1)+i,:,0] = u[:,valid_list[i][1],valid_list[i][0]]
+            data_in[num_per_frame*(frame_count-1)+i,:,1] = v[:,valid_list[i][1],valid_list[i][0]]
+            data_in[num_per_frame*(frame_count-1)+i,:,2] = w[:,valid_list[i][1],valid_list[i][0]]
+            data_in[num_per_frame*(frame_count-1)+i,:,3] = th[:,valid_list[i][1],valid_list[i][0]]
+            data_in[num_per_frame*(frame_count-1)+i,:,4] = qv[:,valid_list[i][1],valid_list[i][0]]
+            data_out[num_per_frame*(frame_count-1)+i,:,0] = uw[:,valid_list[i][1],valid_list[i][0]]
+            data_out[num_per_frame*(frame_count-1)+i,:,1] = vw[:,valid_list[i][1],valid_list[i][0]]
+            data_out[num_per_frame*(frame_count-1)+i,:,2] = thw[:,valid_list[i][1],valid_list[i][0]]
+            data_out[num_per_frame*(frame_count-1)+i,:,3] = qvw[:,valid_list[i][1],valid_list[i][0]]
+
+        print('Task '+str(hid)+':'+str(mid)+'0:00 is finished')
 		
-		print('Task '+str(hid)+':'+str(mid)+'0:00 is finished')
-		
-		if hid == 23:
-			break
+        if hid == 23:
+            break
 	
 	if hid == 23:
-		break
+            break
 
 # save data_in as raw input and output (not normalized)
 # we can choose to normalize directly to get simple normalized data
@@ -100,28 +100,28 @@ large_std_all = np.zeros([5,])
 turflux_avg_all = np.zeros([4,])
 turflux_std_all = np.zeros([4,])
 for n in range(5):
-	for h in range(nz):
-		large_avg[h,n]=np.sum(data_in[:train_vali_num,h,n],axis=None)/train_vali_num
+    for h in range(nz):
+        large_avg[h,n]=np.sum(data_in[:train_vali_num,h,n],axis=None)/train_vali_num
 for n in range(4):
-	for h in range(nz):
-		turflux_avg[h,n]=np.sum(data_out[:train_vali_num,h,n],axis=None)/train_vali_num
+    for h in range(nz):
+        turflux_avg[h,n]=np.sum(data_out[:train_vali_num,h,n],axis=None)/train_vali_num
 
 for n in range(5):
-	for h in range(nz):
-		large_std[h,n]=np.sqrt(np.sum(np.square(data_in[:train_vali_num,h,n]-large_avg[h,n]))/train_vali_num)
+    for h in range(nz):
+        large_std[h,n]=np.sqrt(np.sum(np.square(data_in[:train_vali_num,h,n]-large_avg[h,n]))/train_vali_num)
 for n in range(4):
-	for h in range(nz):
-		turflux_std[h,n]=np.sqrt(np.sum(np.square(data_out[:train_vali_num,h,n]-turflux_avg[h,n]))/train_vali_num)
+    for h in range(nz):
+        turflux_std[h,n]=np.sqrt(np.sum(np.square(data_out[:train_vali_num,h,n]-turflux_avg[h,n]))/train_vali_num)
 
 for n in range(5):
-	large_avg_all[n] = np.sum(data_in[:train_vali_num,:,n],axis=None)/(train_vali_num*nz)
+    large_avg_all[n] = np.sum(data_in[:train_vali_num,:,n],axis=None)/(train_vali_num*nz)
 for n in range(4):
-	turflux_avg_all[n] = np.sum(data_out[:train_vali_num,:,n],axis=None)/(train_vali_num*nz)
+    turflux_avg_all[n] = np.sum(data_out[:train_vali_num,:,n],axis=None)/(train_vali_num*nz)
 
 for n in range(5):
-	large_std_all[n] = np.sqrt(np.sum(np.square(data_in[:train_vali_num,:,n]-large_avg_all[n]),axis=None)/(train_vali_num*nz))
+    large_std_all[n] = np.sqrt(np.sum(np.square(data_in[:train_vali_num,:,n]-large_avg_all[n]),axis=None)/(train_vali_num*nz))
 for n in range(4):
-	turflux_std_all[n] = np.sqrt(np.sum(np.square(data_out[:train_vali_num,:,n]-turflux_avg_all[n]),axis=None)/(train_vali_num*nz))
+    turflux_std_all[n] = np.sqrt(np.sum(np.square(data_out[:train_vali_num,:,n]-turflux_avg_all[n]),axis=None)/(train_vali_num*nz))
 
 # save the stds and avgs
 np.save(output_path+'large_avg',large_avg)
